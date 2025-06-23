30a035abc7d5427333e5af3ca67c393f272e5224
Beherbest <valapo5576@nutrv.com>
2025-06-23 15:20:35 +0300

    done

--- Website/myproject/settings.py
+++ Website/myproject/settings.py
@@ -5,7 +5,11 @@ import dj_database_url
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 # Hardcode for Render deployment
-ALLOWED_HOSTS = ['be-her-best-1.onrender.com', '.onrender.com']
+ALLOWED_HOSTS = [
+    'be-her-best-1.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
@@ -19,3 +23,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 } 
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
