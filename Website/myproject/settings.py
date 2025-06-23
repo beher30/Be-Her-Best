cf8c45163ccffbe1c755b2f17e827f884d15bfa5
Beherbest <valapo5576@nutrv.com>
2025-06-23 14:32:14 +0300

    updated allowed host

--- Website/myproject/settings.py
+++ Website/myproject/settings.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'be-her-best-1.onrender.com').split(',')
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
