4b25c0ffc72ef8908c71844f1b47265a1e4850d9
Beherbest <valapo5576@nutrv.com>
2025-06-25 16:17:50 +0300

    Move build.sh to Website/myproject for Render

---
+++ Website/myproject/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Run the cache clearing script
+./clear-build-cache.sh
+
+pip install -r requirements.txt
+
+cd Website/myproject
+python manage.py collectstatic --noinput
+python manage.py migrate 
