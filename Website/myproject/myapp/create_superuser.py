9f32bea27fa567f1d3ffbea50fec8aa382b2dd60
Beherbest <valapo5576@nutrv.com>
2025-06-25 16:45:05 +0300

    done

---
+++ Website/myproject/myapp/create_superuser.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+if not User.objects.filter(username="beherbest").exists():
+    User.objects.create_superuser("beherbest", "beherbest@gmail.com", "beherbestfuck")
+    print("Superuser created.")
+else:
+    print("Superuser already exists.") 
